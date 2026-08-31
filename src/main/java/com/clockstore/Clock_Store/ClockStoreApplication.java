1f605ae3fd3338e2ad5af326e8b3b718ce273dfd
NesrinJebali <hopehorizon62@gmail.com>
2026-08-31 17:35:34 +0100

    first commit

---
+++ src/main/java/com/clockstore/Clock_Store/ClockStoreApplication.java
@@ -0,0 +1,13 @@
+package com.clockstore.Clock_Store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClockStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClockStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clockstore/Clock_Store/ClockStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clockstore.Clock_Store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClockStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
